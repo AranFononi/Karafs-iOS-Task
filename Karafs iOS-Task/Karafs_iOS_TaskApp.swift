5aa524c53bd0f0f22d1e38bae99b75e16061df1b
Aran Fononi <aaronfononi@gmail.com>
2025-04-09 16:49:58 +0330

    Initial Commit

---
+++ Karafs iOS-Task/Karafs_iOS_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Karafs_iOS_TaskApp.swift
+//  Karafs iOS-Task
+//
+//  Created by Aran Fononi on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Karafs_iOS_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
